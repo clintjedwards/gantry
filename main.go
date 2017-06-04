bde85b2f22c12777b045d33988cb06821aa97414
Clint Edwards <clint.j.edwards@gmail.com>
2017-06-04 02:04:07 -0400

    Add configuration file parsing and shifting directions

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 // Gantry is a tool for manipulating docker-compose infrastructure using ssh
 
+//  Idea is to create a tool that simply ssh's into a server
+//  creates an ssh tunnel for you and then drops you into a shell with the right
+//  docker variables set. This would enable you to run normal docker commands on
+//  the remote host and wouldn't have to recreate commands
+
 package main
+
+import "fmt"
 
 func main() {
 
     argument_parse()
+    config := load_config()
+
+    fmt.Println(config)
 
 }
---
+++ ssh.go
@@ -0,0 +1,6 @@
+//Provides functions to create local port,
+//                        ssh into remote server,
+//                        connect to docker socket and
+//                        spawn terminal that has the proper docker env variables to talk to daemon
+
+package main
