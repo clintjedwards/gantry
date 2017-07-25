810e316f8250823935586a0644d4687013abd28a
Clint Edwards <clint.j.edwards@gmail.com>
2017-07-25 18:24:50 -0400

    Moving bashfile contents to seperate variable and file

--- shell.go
+++ shell.go
@@ -11,54 +11,7 @@ import (
 
 func createTmpBashrc() *os.File {
 
-	content := []byte(`
-		#Prompt settings
-		PS1="\[\e[34m[\$GANTRY_HOST]\] gantry $ \[\e[0m\]"
-
-		#Shortcuts
-		function deploy() {
-    		{
-				docker-compose -f docker-compose.production.yml pull &&
-    			docker-compose -f docker-compose.production.yml down &&
-				docker-compose -f docker-compose.production.yml up -d
-			} || {
-				echo "Command failed: Make sure you're in your project's correct docker directory"
-				echo "Gantry expects compose file named: docker-compose.production.yml"
-			}
-		}
-
-		function push() {
-			{
-    			docker-compose -f docker-compose.production.yml pull &&
-				docker-compose -f docker-compose.production.yml up -d
-			} || {
-				echo "Command failed: Make sure you're in your project's correct docker directory"
-				echo "Gantry expects compose file named: docker-compose.production.yml"
-			}
-		}
-
-		function help(){
-			echo "Help:"
-			echo "============"
-			echo "gantry deploy - Pull, full shutdown, and restart of environment"
-			echo "gantry push   - Pull and quick restart of environment"
-		}
-
-		function gantry(){
-			case "$1" in
-			"deploy")
-				deploy
-				;;
-			"push")
-				push
-				;;
-			*)
-				help
-				;;
-			esac
-		}
-
-	`)
+	content := []byte(bashrcContent)
 
 	tmpfile, err := ioutil.TempFile("", "bashrc")
 	if err != nil {
