c9689f03af2a6773c97ea8c87a5ab5ff88d7d152
Clint Edwards <clint.j.edwards@gmail.com>
2017-07-26 02:26:42 -0400

    If username is not provided in serverurl string, username is retrieved from os
    Fixes #5

--- parser.go
+++ parser.go
@@ -2,8 +2,13 @@
 
 package main
 
-import "strings"
-import "gopkg.in/alecthomas/kingpin.v2"
+import (
+	"log"
+	"os/user"
+	"strings"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
 
 // Arguments represents storage object for command-line parsed variables.
 type Arguments struct {
@@ -14,17 +19,30 @@ type Arguments struct {
 
 func argumentParse() Arguments {
 
-	var serverURL = kingpin.Arg("serverURL", "Server to connect to in format: user@example.com").Required().String()
+	var serverURL = kingpin.Arg("serverURL", "Server to connect to in format: user@example.com or example.com").Required().String()
 	var port = kingpin.Arg("port", "Local port to bind to. Will bind to 9876 if not specified").Default("9876").String()
 
 	kingpin.Parse()
 
 	arguments := Arguments{}
 
-	serverURLSlice := strings.Split(*serverURL, "@")
+	if strings.Contains(*serverURL, "@") {
 
-	arguments.remoteUsername = serverURLSlice[0]
-	arguments.remoteURL = serverURLSlice[1]
+		serverURLSlice := strings.Split(*serverURL, "@")
+
+		arguments.remoteUsername = serverURLSlice[0]
+		arguments.remoteURL = serverURLSlice[1]
+
+	} else {
+		user, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		arguments.remoteUsername = user.Username
+		arguments.remoteURL = *serverURL
+	}
+
 	arguments.localPort = *port
 
 	return arguments
