a42e7154005e3f12b45af559a3edbda4202c7415
Clint Edwards <clint.j.edwards@gmail.com>
2017-09-03 04:47:20 -0400

    Breaking out executor functions

--- executor.go
+++ executor.go
@@ -7,6 +7,31 @@ import (
 	"strings"
 )
 
+func fullDeploy() {
+	command1 := fmt.Sprintf("docker-compose -f docker-compose.production.yml pull")
+	command2 := fmt.Sprintf("docker-compose -f docker-compose.production.yml down")
+	command3 := fmt.Sprintf("docker-compose -f docker-compose.production.yml up -d")
+	cmd := exec.Command("/bin/sh", "-c", command1, "&&", command2, "&&", command3)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Got error: %s\n", err.Error())
+	}
+}
+
+func halfDeploy() {
+	command1 := fmt.Sprintf("docker-compose -f docker-compose.production.yml pull")
+	command2 := fmt.Sprintf("docker-compose -f docker-compose.production.yml up -d")
+	cmd := exec.Command("/bin/sh", "-c", command1, "&&", command2)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Got error: %s\n", err.Error())
+	}
+}
+
 func executor(input string) {
 	input = strings.TrimSpace(input)
 
@@ -20,26 +45,9 @@ func executor(input string) {
 		os.Exit(0)
 		return
 	case "full-deploy":
-		command1 := fmt.Sprintf("docker-compose -f docker-compose.production.yml pull")
-		command2 := fmt.Sprintf("docker-compose -f docker-compose.production.yml down")
-		command3 := fmt.Sprintf("docker-compose -f docker-compose.production.yml up -d")
-		cmd := exec.Command("/bin/sh", "-c", command1, "&&", command2, "&&", command3)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			fmt.Printf("Got error: %s\n", err.Error())
-		}
+		fullDeploy()
 	case "half-deploy":
-		command1 := fmt.Sprintf("docker-compose -f docker-compose.production.yml pull")
-		command2 := fmt.Sprintf("docker-compose -f docker-compose.production.yml up -d")
-		cmd := exec.Command("/bin/sh", "-c", command1, "&&", command2)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			fmt.Printf("Got error: %s\n", err.Error())
-		}
+		halfDeploy()
 	default:
 		cmd := exec.Command("/bin/sh", "-c", input)
 		cmd.Stdin = os.Stdin
